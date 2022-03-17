55abe28c893a23248ea2ee57beb920fab0e04c6e
jbenaventem <jesus.benavente@gruposantander.com>
2022-03-17 18:07:43 +0100

    feat(#1): Include cloud terraform dependency

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  cloud {
+    organization = "jbenaventem"
+
+    workspaces {
+      name = "terraform-test-github-actions"
+    }
+  }
+}
 resource "random_pet" "generate_id" {
   length    = 1
   prefix    = "Fone Bone"
